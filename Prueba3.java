3bb4d3cf8edf0a116dd7145a5743e97db8dfc758
Tobias Choclin <tchoclin@uade.edu.ar>
2025-03-17 15:27:41 -0300

    Agrego java

---
+++ Prueba3.java
@@ -0,0 +1,12 @@
+package prueba;
+public class Prueba3 {
+    public static void main(String[] args) {
+        // Declarar e inicializar el vector
+        int[] vector = {1, 2, 3};
+
+        // Bucle para imprimir los elementos del vector
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println(vector[i]);
+        }
+    }
+}
